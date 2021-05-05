6e80d0fd43349f0e7b724cbf15a558d0398a5e2d
Atiqurrahmanshawon <shawoncsebitc@gmail.com>
2021-05-05 10:28:42 +0600

    commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function home()
+    {
+        return view('admin.index');
+    }
+
+    public function dashboard()
+    {
+        return view('admin.dashboard');
+    }
+
+    public function blog()
+    {
+        return view('admin.blog');
+    }
+
+    public function service()
+    {
+        return view('admin.service');
+    }
+
+    public function lawyer()
+    {
+        return view('admin.lawyer');
+    }
+
+    public function category()
+    {
+        return view('admin.category');
+    }
+    public function case()
+    {
+        return view('admin.case');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,23 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-// Route::get('/','Homecontroller@master')->name('master');
-Route::get('/','Homecontroller@index')->name('index');
-Route::get('/about','Homecontroller@about')->name('about');
-Route::get('/contact','Homecontroller@contact')->name('contact');
+// Route::get('/','myController@master')->name('master');
+Route::get('/index','myController@index')->name('index');
+Route::get('/about','myController@about')->name('about');
+Route::get('/service','myController@service')->name('service');
+Route::get('/caseStudy','myController@case')->name('case');
+Route::get('/blog','myController@blog')->name('blog');
+Route::get('/contact','myController@contact')->name('contact');
+Route::get('/appointment','myController@appointment')->name('appointment');
+
+Auth::routes();
+
+Route::get('/home', 'Homecontroller@index')->name('home');
+
+Route::get('/admin','AdminController@home');
+Route::get('/admin/main','AdminController@dashboard')->name('main');
+Route::get('/admin/add-blog','AdminController@blog')->name('add-blog');
+Route::get('/admin/add-service','AdminController@service')->name('add-service');
+Route::get('/admin/add-lawyer','AdminController@lawyer')->name('add-lawyer');
+Route::get('/admin/add-category','AdminController@category')->name('add-category');
+Route::get('/admin/add-case-study','AdminController@case')->name('add-case-study');
